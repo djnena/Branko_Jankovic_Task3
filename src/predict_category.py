c0eaf4d24becff27b18710784b441041854f4f84
Branko <jankovicb1003@gmail.com>
2026-08-07 22:55:22 +0200

    modifi predict_category

--- src/predict_category.py
+++ src/predict_category.py
@@ -2,7 +2,7 @@ import joblib
 import pandas as pd
  
 # Load the saved model
-model = joblib.load("Branko_Jankovic_Task3/model/final_linear_svm_model.pkl")
+model = joblib.load("model/final_linear_svm_model.pkl")
  
 print("Model loaded successfully!")
 print("Type 'exit' at any point to stop.\n")
--- src/train_model.py
+++ src/train_model.py
@@ -5,7 +5,7 @@ from sklearn.compose import ColumnTransformer
 from sklearn.pipeline import Pipeline
 from sklearn.svm import LinearSVC
 
-df=pd.read_csv("Branko_Jankovic_Task3/data/products.csv")
+df=pd.read_csv("data/products.csv")
 
 #sklanjanje vodecih ili zavrsnih razmaka
 df.columns = df.columns.str.strip()
@@ -40,6 +40,6 @@ final_model = Pipeline([
 final_model.fit(X, y)
 
 # Čuvanje modela i vektorizatora
-joblib.dump(final_model, "Branko_Jankovic_Task3/model/final_linear_svm_model.pkl")
+joblib.dump(final_model, "model/final_linear_svm_model.pkl")
 
 print("Sačuvan fajl: final_linear_svm_model.pkl")
